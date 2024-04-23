7b086a1e2f7539c54191e00265177d140b95525b
Madeo Juan <juan.j.madeo@ar.ey.com>
2024-04-23 15:34:38 -0300

    Update initDB.sql

--- volumenes/initDB.sql
+++ volumenes/initDB.sql
@@ -30,6 +30,14 @@ CREATE TABLE `codigo_moneda` (
   `simbolo` varchar(45) NOT NULL,
   PRIMARY KEY (`cod_moneda`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `codigo_moneda` (`cod_moneda`, `pais`, `simbolo`) VALUES
+(1, 'Estados Unidos', 'USD'),
+(2, 'Eurozona', 'EUR'),
+(3, 'Japón', 'JPY'),
+(4, 'Reino Unido', 'GBP'),
+(5, 'Australia', 'AUD');
+
+
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -83,11 +91,25 @@ CREATE TABLE `estado_cuenta` (
   `detalle` varchar(45) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+INSERT INTO `estado_cuenta` (`id`, `detalle`) VALUES
+(1, 'Activa'),
+(2, 'Inactiva'),
+(3, 'Suspendida'),
+(4, 'Cancelada'),
+(5, 'Bloqueada');
+
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `estado_cuenta`
 --
+
+INSERT INTO `cuentas` (`numcue`, `persnum`, `divisa`, `estado`, `saldo`) VALUES
+('1234567890', 1, 1, 1, 10000),
+('9876543210', 2, 2, 1, 5000),
+('4567890123', 3, 3, 1, 8000),
+('7890123456', 4, 4, 1, 3000),
+('6543210987', 5, 5, 1, 12000);
 
 LOCK TABLES `estado_cuenta` WRITE;
 /*!40000 ALTER TABLE `estado_cuenta` DISABLE KEYS */;
@@ -126,6 +148,15 @@ USE `personas`;
 --
 -- Table structure for table `domicilio`
 --
+
+
+
+
+
+
+
+
+
 
 DROP TABLE IF EXISTS `domicilio`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -223,6 +254,37 @@ CREATE TABLE `usuarios` (
 --
 -- Dumping data for table `usuarios`
 --
+
+INSERT INTO `estado_usuario` (`idestado_usuario`, `descripcion`) VALUES
+(1, 'Activo'),
+(2, 'Inactivo'),
+(3, 'Suspendido'),
+(4, 'Bloqueado'),
+(5, 'Cancelado');
+
+INSERT INTO `tipo_usuario` (`idtipo_usuario`, `descripcion`) VALUES
+(1, 'Cliente'),
+(2, 'Empleado'),
+(3, 'Administrador'),
+(4, 'Proveedor'),
+(5, 'Invitado');
+
+
+INSERT INTO `usuarios` (`persnum`, `nombre`, `apellido`, `dni`, `estado`, `tipo`) VALUES
+(1, 'John', 'Doe', '12345678', 1, 1),
+(2, 'Marie', 'Curie', '23456789', 1, 2),
+(3, 'Ichiro', 'Suzuki', '34567890', 1, 3),
+(4, 'Winston', 'Churchill', '45678901', 1, 4),
+(5, 'Steve', 'Irwin', '56789012', 1, 5);
+
+
+INSERT INTO `domicilio` (`iddomicilio`, `persnum`, `calle`, `numero`, `provincia`, `localidad`) VALUES
+(1, 1, 'Main Street', '123', 'California', 'Los Angeles'),
+(2, 2, 'Rue de la Paix', '456', 'Île-de-France', 'Paris'),
+(3, 3, 'Ginza', '789', NULL, 'Tokyo'),
+(4, 4, 'Downing Street', '10', 'England', 'London'),
+(5, 5, 'George Street', '456', 'New South Wales', 'Sydney');
+
 
 LOCK TABLES `usuarios` WRITE;
 /*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
@@ -332,79 +394,6 @@ CREATE TABLE `tarjetas` (
 -- Dumping data for table `tarjetas`
 --
 
-LOCK TABLES `tarjetas` WRITE;
-/*!40000 ALTER TABLE `tarjetas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `tarjetas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-04-12 13:13:01
-
-
-INSERT INTO `codigo_moneda` (`cod_moneda`, `pais`, `simbolo`) VALUES
-(1, 'Estados Unidos', 'USD'),
-(2, 'Eurozona', 'EUR'),
-(3, 'Japón', 'JPY'),
-(4, 'Reino Unido', 'GBP'),
-(5, 'Australia', 'AUD');
-
-
-INSERT INTO `cuentas` (`numcue`, `persnum`, `divisa`, `estado`, `saldo`) VALUES
-('1234567890', 1, 1, 1, 10000),
-('9876543210', 2, 2, 1, 5000),
-('4567890123', 3, 3, 1, 8000),
-('7890123456', 4, 4, 1, 3000),
-('6543210987', 5, 5, 1, 12000);
-
-
-INSERT INTO `estado_cuenta` (`id`, `detalle`) VALUES
-(1, 'Activa'),
-(2, 'Inactiva'),
-(3, 'Suspendida'),
-(4, 'Cancelada'),
-(5, 'Bloqueada');
-
-
-INSERT INTO `domicilio` (`iddomicilio`, `persnum`, `calle`, `numero`, `provincia`, `localidad`) VALUES
-(1, 1, 'Main Street', '123', 'California', 'Los Angeles'),
-(2, 2, 'Rue de la Paix', '456', 'Île-de-France', 'Paris'),
-(3, 3, 'Ginza', '789', NULL, 'Tokyo'),
-(4, 4, 'Downing Street', '10', 'England', 'London'),
-(5, 5, 'George Street', '456', 'New South Wales', 'Sydney');
-
-
-INSERT INTO `estado_usuario` (`idestado_usuario`, `descripcion`) VALUES
-(1, 'Activo'),
-(2, 'Inactivo'),
-(3, 'Suspendido'),
-(4, 'Bloqueado'),
-(5, 'Cancelado');
-
-
-INSERT INTO `tipo_usuario` (`idtipo_usuario`, `descripcion`) VALUES
-(1, 'Cliente'),
-(2, 'Empleado'),
-(3, 'Administrador'),
-(4, 'Proveedor'),
-(5, 'Invitado');
-
-
-
-INSERT INTO `usuarios` (`persnum`, `nombre`, `apellido`, `dni`, `estado`, `tipo`) VALUES
-(1, 'John', 'Doe', '12345678', 1, 1),
-(2, 'Marie', 'Curie', '23456789', 1, 2),
-(3, 'Ichiro', 'Suzuki', '34567890', 1, 3),
-(4, 'Winston', 'Churchill', '45678901', 1, 4),
-(5, 'Steve', 'Irwin', '56789012', 1, 5);
-
 
 INSERT INTO `estado_tarjeta` (`id`, `detalle`) VALUES
 (1, 'Activa'),
@@ -428,3 +417,31 @@ INSERT INTO `tarjetas` (`numtarj`, `numcue`, `f_vencimiento`, `pin`, `estado`, `
 ('3333444455556666', 3, '07/28', 3456, 1, '04/24', 'C'),
 ('4444555566667777', 4, '08/28', 4567, 1, '04/24', 'C'),
 ('5555666677778888', 5, '09/28', 5678, 1, '04/24', 'C');
+
+
+LOCK TABLES `tarjetas` WRITE;
+/*!40000 ALTER TABLE `tarjetas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tarjetas` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-04-12 13:13:01
+
+
+
+
+
+
+
+
+
+
+
